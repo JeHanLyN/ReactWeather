2da7522f96e959af3c9d8097ffe623f1bd88a886
bullet yap <bu11et@rsm.rubicon.ph>
2017-05-26 18:14:55 +0800

    Update middleware for localhost

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ var app = express();
 const PORT = process.env.PORT || 3000; //ang const parehas lang ng var ang pinagkaiba lang ang const hndi pde palitan ng value
 
 app.use(function (req, res, next) { //kung https ililipat sa https kc ung https may bayad
-  if (req.headers['x-forwarded-proto'] === 'http') {
-    next();
-  }else{
+  if (req.headers['x-forwarded-proto'] === 'https') {
     res.redirect('http://' + req.hostname + req.url);
+  }else{
+    next();
   }
 });
 //paggumagamit ng const common convention is use UPPERCASE
